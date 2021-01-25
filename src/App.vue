fb35a43f32f593155fd26e08f6a8c6b6991cafce
akbarhps <akbarputrasrg@gmail.com>
2021-01-25 10:23:26 +0700

    🚀 Implement app navbar

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,34 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <Navbar/>
   <router-view/>
 </template>
 
+<script>
+import Navbar from "@/components/Navbar";
+
+export default {
+  components: {Navbar}
+}
+</script>
+
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Josefin+Sans');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Josefin Sans", Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
+.navbar {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 10px;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.base-layout {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 10px;
 }
 </style>
