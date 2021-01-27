57f8105f59d15302859eaee8f3b581ab8910cfe3
akbarhps <akbarputrasrg@gmail.com>
2021-01-27 19:24:29 +0700

    🧹 Refactor Dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,30 +1,50 @@
 <template>
-  <h1>Dashboard</h1>
-  <button @click="logUserOut">Delete cookies</button>
+  <div class="base-layout">
+    <button @click="logUserOut">Log out</button>
+    <!--    <Navbar/>-->
+    <!--    <div class="layout">-->
+    <!--      <div class="body__content">-->
+    <!--        <Assignment v-for="assignment in assignmentList" :key="assignment.id" :assignment="assignment"/>-->
+    <!--      </div>-->
+    <!--    </div>-->
+    <!--    <Footer/>-->
+  </div>
 </template>
 
 <script>
-import cookiesHandler from "@/composables/cookiesHandler";
 import {useRouter} from "vue-router";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import Assignment from "@/components/Assignment";
+import apiHandler from "@/composables/apiHandler";
+import {getCookies, deleteCookies} from "@/composables/cookiesHandler";
 
 export default {
-  name: "Dashboard",
+  components: {Footer, Navbar, Assignment},
   setup() {
     const router = useRouter();
-    const {getCookies, deleteCookies} = cookiesHandler();
+    const {getAssignment, assignmentList, isLoading, error} = apiHandler();
     const cookies = JSON.parse(getCookies());
     console.log(cookies);
 
     function logUserOut() {
-      deleteCookies();
+      deleteCookies()
       router.go(0);
     }
 
-    return {logUserOut}
+    return {assignmentList, logUserOut}
   }
 }
 </script>
 
 <style scoped>
+.base-layout {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 10px;
+}
 
+.layout {
+  height: 80%;
+}
 </style>
